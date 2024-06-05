f9b572ce3d83cfc447ec87d4abcd03114aee005e
Quazzik <yuakorotkov@gmail.com>
2024-06-05 20:24:32 +0500

    added base logic for bot

--- BotHost/BotAPILib/CoreRequests.cs
+++ BotHost/BotAPILib/CoreRequests.cs
@@ -1,11 +1,11 @@
 ﻿namespace BotAPILib
 {
-    public class CoreRequests
+    public static class CoreRequests
     {
-        private readonly HttpClient _httpClient = new();
-        public async Task<string> GetBotTokenAsync(string pass)
+        private static readonly HttpClient _httpClient = new();
+        public static async Task<string> GetBotTokenAsync(string pass)
         {
-            var res = await _httpClient.GetAsync($"http://localhost:6900/api/BotHostAPI/GetToken?pwd={pass}");
+            var res = await _httpClient.GetAsync($"http://localhost:5274/api/BotHostAPI/GetToken?pwd={pass}");
             var token = await res.Content.ReadAsStringAsync();
             return token;
         }
--- BotHost/BotHost/Program.cs
+++ BotHost/BotHost/Program.cs
@@ -1,4 +1,9 @@
-﻿using Telegram.Bot;
+﻿using BotAPILib;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Net;
+using System.Reflection.Metadata;
+using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
@@ -6,9 +11,13 @@ namespace BotHost;
 
 public class Program
 {
+    private static TelegramBotClient _botClient;
+    private static readonly HttpClient _httpClient = new();
+    private static readonly InputFile errorSticker = InputFile.FromString("CAACAgIAAxkBAAEF7KFmYH1DPjnUE7L5aqRhpc14nyANPwACEDwAAt-BkUpgkS6UfM10vTUE");
+
     private static List<string> _commandsAvailable = new()
     {
-        "/someMessage"
+        "/start"
     };
 
     static void Main(string[] args)
@@ -16,13 +25,47 @@ public class Program
         MainAsync().GetAwaiter().GetResult();
     }
 
-<<<<<<< Updated upstream
-=======
     private static async Task MainAsync()
     {
-
+        var token = await CoreRequests.GetBotTokenAsync("v4)xh_VN>%539~r:J6sYD7");
+        _botClient = new TelegramBotClient(token, _httpClient);
+        _botClient.StartReceiving(UpdateHandler, ErrorHandler);
+        Console.ReadLine();
     }
 
->>>>>>> Stashed changes
+    private static async Task ErrorHandler(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 
+    private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        await ProcedureMessageAsync(update);
+    }
+
+    private static async Task ProcedureMessageAsync(Update update)
+    {
+        if (update.Message != null && update.Message.Entities.Any())
+        {
+            foreach (var entity in update.Message.Entities)
+                await ProcedureCommand(update.Message, entity);
+        }
+    }
+
+    private static async Task ProcedureCommand (Message message, MessageEntity entity)
+    {
+        if (entity.Type != MessageEntityType.BotCommand)
+            await _botClient.SendStickerAsync(
+                chatId: message.Chat.Id,
+                sticker: errorSticker
+                );
+            await _botClient.SendTextMessageAsync(message.Chat, "Введите команду");
+
+        var command = string.Join("", message.Text.Skip(entity.Offset).Take(entity.Length));
+
+        if (!_commandsAvailable.Contains(command))
+        {
+
+        }
+    }
 }
