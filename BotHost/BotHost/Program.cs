f442ccaec72571db2849f611eaf05b24bb77e2ca
Nameless10000 <evgeny.antonov.2004@gmail.com>
2024-06-05 18:54:44 +0500

    BotHost Added

---
+++ BotHost/BotHost/Program.cs
@@ -0,0 +1,13 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace BotHost;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
