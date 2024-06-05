343f95ca13f3643ad50c15f8fa62ae07be0796da
Quazzik <yuakorotkov@gmail.com>
2024-06-05 22:25:00 +0500

    Added CoreRequests

--- BotHost/BotAPILib/CoreRequests.cs
+++ BotHost/BotAPILib/CoreRequests.cs
@@ -1,13 +1,31 @@
-﻿namespace BotAPILib
+﻿using BotApi.Models.DbEntities;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace BotAPILib
 {
     public static class CoreRequests
     {
         private static readonly HttpClient _httpClient = new();
+
         public static async Task<string> GetBotTokenAsync(string pass)
         {
             var res = await _httpClient.GetAsync($"http://localhost:5274/api/BotHostAPI/GetToken?pwd={pass}");
             var token = await res.Content.ReadAsStringAsync();
             return token.Replace("\"", "");
+        }
+
+        public static async Task<List<Worker>> GetWorkers()
+        {
+            var res = await _httpClient.GetAsync("http://localhost:5274/api/BotHostAPI/GetWorkers");
+            var result = await res.Content.ReadFromJsonAsync<List<Worker>>();
+            return result;
+        }
+        public static async Task<List<Discipline>> GetDisciplines()
+        {
+            var res = await _httpClient.GetAsync("http://localhost:5274/api/BotHostAPI/GetDisciplines");
+            var result = await res.Content.ReadFromJsonAsync<List<Discipline>>();
+            return result;
         }
     }
 }
--- BotHost/BotHost/Program.cs
+++ BotHost/BotHost/Program.cs
@@ -12,11 +12,16 @@ namespace BotHost;
 public class Program
 {
     private static TelegramBotClient _botClient;
-    private static readonly InputFile errorSticker = InputFile.FromString("CAACAgIAAxkBAAEF7KFmYH1DPjnUE7L5aqRhpc14nyANPwACEDwAAt-BkUpgkS6UfM10vTUE");
+    private static readonly InputFile _notCommandSticker = InputFile.FromString("CAACAgIAAxkBAAEF7QNmYI5H6op8eacWz-5U5QOSnNlB-QACsToAAg-IcEqRrCbJQ4Uv9TUE");
+    private static readonly InputFile _badCommandSticker = InputFile.FromString("CAACAgIAAxkBAAEF7QlmYI_dRm2j14b-2m1vRgOFKaBwxwAC4j0AAl-PaUqyeSmkhxNJCDUE");
+    private static readonly InputFile _nononoMisterFishSticker = InputFile.FromString("CAACAgIAAxkBAAEF7RtmYJKd7w_ZLJCbyjjSu1HM9HNFEgACbDcAAhBMkEpX9H_SMikrKjUE");
 
     private static List<string> _commandsAvailable = new()
     {
-        "/start"
+        "/workers",
+        "/disciplines",
+        "/appoint",
+        "/workersByDiscipline"
     };
 
     static void Main(string[] args)
@@ -26,8 +31,9 @@ public class Program
 
     private static async Task MainAsync()
     {
-        var token = await CoreRequests.GetBotTokenAsync("qwerty");
+        var token = "6752858428:AAF4gAdbnkVzOeYnPlyaqbs02J0vYovFpdE"; //await CoreRequests.GetBotTokenAsync("qwerty");
         _botClient = new TelegramBotClient(token);
+
         _botClient.StartReceiving(UpdateHandler, ErrorHandler);
         Console.ReadLine();
     }
@@ -44,20 +50,54 @@ public class Program
 
     private static async Task ProcedureMessageAsync(Update update)
     {
-        if (update.Message != null)
+        if (update.Message.Entities != null && update.Message.Entities.Any())
         {
-            await ProcedureCommand(update.Message);
+            if (update.Message.Entities.Length > 1)
+            {
+                await _botClient.SendTextMessageAsync(update.Message.Chat, "Не больше одного запроса за раз!");
+                await _botClient.SendStickerAsync(update.Message.Chat, _nononoMisterFishSticker);
+            }
+            else
+            {
+                await ProcedureCommand(update.Message, update.Message.Entities[0]);
+            }
         }
+        else if (update.Message != null)
+            await ProcedureCommand(update.Message);
     }
 
     private static async Task ProcedureCommand (Message message, MessageEntity entity = null)
     {
         if (entity == null || entity.Type != MessageEntityType.BotCommand)
+        {
             await _botClient.SendStickerAsync(
                 chatId: message.Chat.Id,
-                sticker: errorSticker
+                sticker: _notCommandSticker
                 );
-            await _botClient.SendTextMessageAsync(message.Chat, "Введите команду");
+            await _botClient.SendTextMessageAsync(message.Chat, "Я тут для команд вообще-то!");
+        }
 
+        if (entity != null)
+        {
+            var command = string.Join("", message.Text.Skip(entity.Offset).Take(entity.Length));
+
+            await (command switch
+            {
+                "/workers" => CoreRequests.GetWorkers(),
+                "/disciplines" => _botClient.SendTextMessageAsync(message.Chat, "disciplines"),
+                "/appoint" => _botClient.SendTextMessageAsync(message.Chat, "appoint"),
+                "/workersByDiscipline" => _botClient.SendTextMessageAsync(message.Chat, "workersByDiscipline"),
+                _ => NotCommandMessage(message)
+            });
+        }
+    }
+
+    private static async Task NotCommandMessage(Message message)
+    {
+        await _botClient.SendTextMessageAsync(message.Chat, "Какая-то хуета, а не команда");
+        await _botClient.SendStickerAsync(
+            chatId: message.Chat.Id,
+            sticker: _badCommandSticker
+            );
     }
 }
