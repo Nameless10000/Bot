d620f91b8580f84a653a8ce18cb95a1fd92ff4aa
Nameless10000 <evgeny.antonov.2004@gmail.com>
2024-06-06 17:05:26 +0500

    AddCores Added

--- BotApi/Program.cs
+++ BotApi/Program.cs
@@ -13,6 +13,8 @@ var DataBaseSection = builder.Configuration.GetSection("DataBase");
 builder.Services.Configure<BotData>(botSection);
 builder.Services.Configure<DBInfo>(DataBaseSection);
 
+builder.Services.AddCors();
+
 builder.Services.AddDbContext<BotDbContext>(opt => opt.UseMySql("Server=localhost;Database=Bot;Uid=root;Pwd=root;", new MySqlServerVersion(new Version(5,7,24))));
 
 builder.Services.AddTransient<BotHostService>();
