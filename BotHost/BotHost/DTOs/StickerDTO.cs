3e0146a61c39de3b8e37736567caf92986c8a52f
Nameless10000 <evgeny.antonov.2004@gmail.com>
2024-06-08 14:59:57 +0500

    TODO added

--- BotHost/BotHost/DTOs/StickerDTO.cs
+++ BotHost/BotHost/DTOs/StickerDTO.cs
@@ -19,13 +19,13 @@ namespace BotHost.DTOs
     public class StickerDTO
     {
         private static readonly Dictionary<StickerType, InputFile> _stickers = new Dictionary<StickerType, InputFile>
-    {
-        { StickerType.NotCommand, InputFile.FromString("CAACAgIAAxkBAAEF7QNmYI5H6op8eacWz-5U5QOSnNlB-QACsToAAg-IcEqRrCbJQ4Uv9TUE") },
-        { StickerType.BadCommand, InputFile.FromString("CAACAgIAAxkBAAEF7QlmYI_dRm2j14b-2m1vRgOFKaBwxwAC4j0AAl-PaUqyeSmkhxNJCDUE") },
-        { StickerType.NononoMisterFish, InputFile.FromString("CAACAgIAAxkBAAEF7RtmYJKd7w_ZLJCbyjjSu1HM9HNFEgACbDcAAhBMkEpX9H_SMikrKjUE") },
-        { StickerType.Hello, InputFile.FromString("CAACAgIAAxkBAAEF7aZmYK-BDiV_pQahDj5OxOIlJRQulQACxjAAAluPkEqow5iNYORvuTUE") },
-        { StickerType.WorkInProgress, InputFile.FromString("CAACAgIAAxkBAAEF7cBmYLMUFbjj-qDvJoOVzzxXOIDrJQAChEMAAp4daEqVzKX5VFmspjUE") }
-    };
+        {
+            { StickerType.NotCommand, InputFile.FromString("CAACAgIAAxkBAAEF7QNmYI5H6op8eacWz-5U5QOSnNlB-QACsToAAg-IcEqRrCbJQ4Uv9TUE") },
+            { StickerType.BadCommand, InputFile.FromString("CAACAgIAAxkBAAEF7QlmYI_dRm2j14b-2m1vRgOFKaBwxwAC4j0AAl-PaUqyeSmkhxNJCDUE") },
+            { StickerType.NononoMisterFish, InputFile.FromString("CAACAgIAAxkBAAEF7RtmYJKd7w_ZLJCbyjjSu1HM9HNFEgACbDcAAhBMkEpX9H_SMikrKjUE") },
+            { StickerType.Hello, InputFile.FromString("CAACAgIAAxkBAAEF7aZmYK-BDiV_pQahDj5OxOIlJRQulQACxjAAAluPkEqow5iNYORvuTUE") },
+            { StickerType.WorkInProgress, InputFile.FromString("CAACAgIAAxkBAAEF7cBmYLMUFbjj-qDvJoOVzzxXOIDrJQAChEMAAp4daEqVzKX5VFmspjUE") }
+        };
 
         public static InputFile GetSticker(StickerType stickerType)
         {
